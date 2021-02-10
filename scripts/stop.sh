b8653858a4e422ca7f84e295946740dafb427922
armando <armando.martinez@esausi.com.mx>
2021-02-10 15:57:59 -0600

    Fix ortography on stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 [[ -e /etc/init/helloworld.conf ]] \
    && status helloworld | \
-      grep -q '^ĥelloworld start/running, process' \
+      grep -q '^helloworld start/running, process' \
    && [[ $? -eq 0 ]] \
    && stop helloworld || echo "Application not started"
